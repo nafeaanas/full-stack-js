fc25cb39402ef1f07c9fe91843c77eee3657e34a
nafeaanas <nafeaanass15@gmail.com>
2023-01-02 14:40:10 +0100

    Shazam-Morocco

--- Application-de-Livraison-MARHABA/server/test/api.test.js
+++ Application-de-Livraison-MARHABA/server/test/api.test.js
@@ -4,30 +4,29 @@ const app = require('../server')
 describe("POST /api/auth/login", () => {
     it("should be provide an email and password after login", async () => {
       const res = await request(app).post("/api/auth/login").send({
-        email: 'abdeobettal@gmail.com' ,
+        email: 'nafeaanass15@gmail.com' ,
       });
       expect(res.statusCode).toBe(400);
     });
-    // it("should be login seccufuly", async () => {
-    //   const res = await request(app).post("/api/auth/login").send({
-    //     // name: "abdessamad",
-    //     email: "" ,
-    //     password: ""
-    //   });
-    //   expect(res.statusCode).toBe(200);
-    // });
-    // it("should be not login because not verified", async () => {
-    //   const res = await request(app).post("/api/auth/login").send({
-    //     email: "2"+ email ,
-    //     password: "2"+ password,
-    //   });
-    //   expect(res.statusCode).toBe(401);
-    // });
-    // it("should be not login because data not valid", async () => {
-    //   const res = await request(app).post("/api/auth/login").send({
-    //     email: "test"+ email ,
-    //     password: "test"+ password,
-    //   });
-    //   expect(res.statusCode).toBe(402);
-    // });
+    it("should be login seccufuly", async () => {
+      const res = await request(app).post("/api/auth/login").send({
+        email: "nafeaanass15@gmail.com" ,
+        password: "chemaia1"
+      });
+      expect(res.statusCode).toBe(200);
+    });
+    it("should be not login because not verified", async () => {
+      const res = await request(app).post("/api/auth/login").send({
+        email: "nafeaanass20@gmail.com",
+        password: "123",
+      });
+      expect(res.statusCode).toBe(401);
+    });
+    it("should be not login because data not valid", async () => {
+      const res = await request(app).post("/api/auth/login").send({
+        email: "nafeaanass15@gmail.com",
+        password: "123",
+      });
+      expect(res.statusCode).toBe(402);
+    });
   });
